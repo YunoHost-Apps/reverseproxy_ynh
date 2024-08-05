de887dc42ae2b07b2de809afcc524fd3d201ffea
selfhoster1312 <selfhoster1312@kl.netlib.re>
2024-08-05 17:46:26 +0200

    Don't stop additional components in UDS

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -11,17 +11,17 @@ rp_validate_proxy_path() {
         url_regex='^(http://(127\.[0-9]+\.[0-9]+\.[0-9]+|localhost)|https://.*)(:[0-9]+)?(/.*)?$'
         [[ ! "$proxy_path" =~ $url_regex ]] && ynh_die \
         "For secure reason, you can't use an unencrypted http remote destination couple with ssowat for your reverse proxy: $proxy_path" 1
-    fi
 
-    # Don't allow trailing slash or additional URI components in proxy_path
-    if [[ "$proxy_path" =~ ^https?:// ]]; then
-        res="${proxy_path//[^\/]}"
-        if [[ "${#res}" != "2" ]]; then
-            if [[ "${#res}" = "3" ]] && [[ "$proxy_path" =~ /$ ]]; then
-                # If it's only one trailing slash (no more components), just remove it
-                proxy_path="${proxy_path::-1}"
-            else
-                ynh_die "Reverse proxy URL cannot contain additional slashes or components: $proxy_path" 1
+        # Don't allow trailing slash or additional URI components in proxy_path if not unix domain socket
+        if [[ "$proxy_path" =~ ^https?:// ]]; then
+            res="${proxy_path//[^\/]}"
+            if [[ "${#res}" != "2" ]]; then
+                if [[ "${#res}" = "3" ]] && [[ "$proxy_path" =~ /$ ]]; then
+                    # If it's only one trailing slash (no more components), just remove it
+                    proxy_path="${proxy_path::-1}"
+                else
+                    ynh_die "Reverse proxy URL cannot contain additional slashes or components: $proxy_path" 1
+                fi
             fi
         fi
     fi
